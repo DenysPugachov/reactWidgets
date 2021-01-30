618ade30cd0420fc08ff0bc15b52e887bc847410
Denys <pugachovdenys@gmail.com>
2021-01-30 15:31:05 +0200

    add Search comt for Wiki (FIXME: make a request on first loading without delay)

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,27 +5,13 @@ import "./Search.css";
 
 const Search = () => {
 
-  const [term, setTerm] = useState("programming");
+  const [term, setTerm] = useState("default");
   const [results, setResults] = useState([]);
 
-  // console.log(results);
-
-  const renderResults = results.map(item => {
-    return (
-      <div className="item"
-        key={ item.pageid }>
-        <div className="content">
-          <div className="header">
-            { item.title }
-          </div>
-          <span dangerouslySetInnerHTML={ { __html: item.snippet } }></span>
-        </div>
-      </div>
-    );
-  });
 
 
   useEffect(() => {
+
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
@@ -40,8 +26,45 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
-  }, [term]);
+
+    //FIXME: make a request on first loading without delay;
+    // console.log(term && !results.length);
+    // if (term && !results.length) {
+    //   search();
+    // } else {
+    // }
+
+
+    //make a request after stop typing delay (1000 ms)
+    const typeTimerId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 1000);
+
+    return () => { clearInterval(typeTimerId); };
+
+  }, [term, results.length]);
+
+  const renderResults = results.map(item => {
+    return (
+      <div className="item"
+        key={ item.pageid }
+      >
+        <div className="right floated content">
+          <a className="ui button"
+            href={ `https:\\en.wikipedia.org?curid=${item.pageid}` }
+          >Go</a>
+        </div>
+        <div className="content">
+          <div className="header">
+            { item.title }
+          </div>
+          <span dangerouslySetInnerHTML={ { __html: item.snippet } }></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
